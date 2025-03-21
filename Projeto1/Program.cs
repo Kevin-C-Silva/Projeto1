12921ced1621f104f9e910bf23856dfd828c2238
Kevin-C-Silva <Kevin.silva158@etec.sp.gov.br>
2025-03-21 14:33:17 -0300

    Adicionando o UsuarioRepositorio no program.cs

--- Projeto1/Program.cs
+++ Projeto1/Program.cs
@@ -3,6 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+builder.Services.AddScoped<Projeto1.Repositorio.UsuarioRepositorio>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
